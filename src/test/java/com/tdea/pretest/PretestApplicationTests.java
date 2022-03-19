17e227964c816fa7ea3e0c03cfcbc804a30e384f
090218 <99548119+090218@users.noreply.github.com>
2022-03-19 11:46:06 -0500

    Update PretestApplicationTests.java

--- src/test/java/com/tdea/pretest/PretestApplicationTests.java
+++ src/test/java/com/tdea/pretest/PretestApplicationTests.java
@@ -2,10 +2,40 @@ package com.tdea.pretest;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class PretestApplicationTests {
 
     @Test
     void contextLoads() {
+
+        String dbpasword;
+        String pasword;
+        dbpasword = "fer";
+        pasword = "fer2";
+
+
+        boolean result = UserUtils.isPasswordEqual(dbpasword, pasword);
+
+        assertFalse(result);
+
+
+    }
+    @Test
+    void contextLoads2() {
+
+        String dbpasword;
+        String pasword;
+        dbpasword = "fer";
+        pasword = "fer";
+
+
+        boolean result = UserUtils.isPasswordEqual(dbpasword, pasword);
+
+        assertTrue(result);
+
+
     }
 
 }
